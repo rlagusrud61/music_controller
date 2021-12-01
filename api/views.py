7f2c007e7cfc85f1e9ffe8511a1949e057add219
Hyeon Kyeong Kim <kimhk@inter-actief.net>
2021-12-01 15:59:53 +0100

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+# Create your views here.
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
